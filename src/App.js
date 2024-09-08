6101be57a65d51051de6989347e92761264955e7
RJ060501 <rjrussell605@gmail.com>
2024-09-08 12:19:16 -0600

    Pagination on ProductList.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import Checkout from './components/Checkout';
 
+//TODO: Replace 'your-public-stripe-key' with your actual Stripe public key.
 const stripePromise = loadStripe('your-public-stripe-key');
 
 function App() {
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { listProducts } from '../actions/productActions';
 
@@ -7,9 +7,18 @@ const ProductList = () => {
   const productList = useSelector((state) => state.productList);
   const { products, loading, error } = productList;
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [productsPerPage] = useState(10); // 10 products per page
+
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
+
+  const indexOfLastProduct = currentPage * productsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
+  const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
     <div>
@@ -18,15 +27,24 @@ const ProductList = () => {
       ) : error ? (
         <h3>{error}</h3>
       ) : (
-        <div>
-          {products.map((product) => (
-            <div key={product._id}>
-              <h3>{product.name}</h3>
-              <p>{product.description}</p>
-              <p>${product.price}</p>
-            </div>
-          ))}
-        </div>
+        <>
+          <div>
+            {currentProducts.map((product) => (
+              <div key={product._id}>
+                <h3>{product.name}</h3>
+                <p>{product.description}</p>
+                <p>${product.price}</p>
+              </div>
+            ))}
+          </div>
+          <div className="pagination">
+            {[...Array(Math.ceil(products.length / productsPerPage)).keys()].map((page) => (
+              <button key={page} onClick={() => paginate(page + 1)}>
+                {page + 1}
+              </button>
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
